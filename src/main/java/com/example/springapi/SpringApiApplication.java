0992d9e64de0e32f0bf994291daa4a7ca9e9c566
AlexandreLeonetti <al.leo@wanadoo.fr>
2024-05-31 22:21:27 +0200

    restAPI

---
+++ src/main/java/com/example/springapi/SpringApiApplication.java
@@ -0,0 +1,15 @@
+/* SpringApiApplication.java */
+/* credits : https://www.youtube.com/watch?v=Zo9xQzibp4Y */
+package com.example.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/springapi/api/controller/UserController.java
@@ -0,0 +1,39 @@
+/* UserController.java */
+package com.example.springapi.api.controller;
+
+import com.example.springapi.api.model.User;
+import com.example.springapi.service.UserService;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController{
+
+	private final  UserService userService;
+	
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@GetMapping("/user")
+	public User getUser(@RequestParam Integer id) {
+		Optional<User> user =  userService.getUser(id);
+
+		
+		/*if(user.isPresent()) {
+			return (User) user.get();
+		}
+
+		return null;
+		*/
+		return  (User) user.orElse(null);
+	}
+	
+
+}
